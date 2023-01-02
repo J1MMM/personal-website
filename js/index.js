c03227cd697f5788bb1ef306d40df7d890e04900
J1MMM <jimuelbaraero00@gmail.com>
2023-01-02 17:45:25 +0800

    asd

---
+++ js/index.js
@@ -0,0 +1,44 @@
+// switch dark mode 
+let switchMode = document.getElementById("switch-mode");
+
+const handleSwitchClick = () => {
+  switchMode.classList.toggle("active")
+  document.body.classList.toggle("dark-mode")
+  console.log("jim");
+}
+
+//card functions
+// let activeIndex = 0;
+
+// const groups = document.getElementsByClassName("card-group");
+// const handleLoveClick = () => {
+//   const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
+  
+//   const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+//         nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+        
+//   currentGroup.dataset.status = "after";
+  
+//   nextGroup.dataset.status = "becoming-active-from-before";
+  
+//   setTimeout(() => {
+//     nextGroup.dataset.status = "active";
+//     activeIndex = nextIndex;
+//   });
+// }
+
+// const handleHateClick = () => {
+//   const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
+  
+//   const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+//         nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+  
+//   currentGroup.dataset.status = "before";
+  
+//   nextGroup.dataset.status = "becoming-active-from-after";
+  
+//   setTimeout(() => {
+//     nextGroup.dataset.status = "active";
+//     activeIndex = nextIndex;
+//   });
+// }
